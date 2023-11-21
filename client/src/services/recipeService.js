517085f55b4a7cbeb1cac7d98bd17dae7bb5b02e
Ventsislav Kalapchiyski <yavorov1995@gmail.com>
2023-11-21 14:08:05 +0200

    Recipe service added with create function

---
+++ client/src/services/recipeService.js
@@ -0,0 +1,16 @@
+const baseUrl = 'http://localhost:3030/jsonstore'
+
+
+export const create = async (recipeData) => {
+    const response = await fetch(`${baseUrl}/recipes`, {
+        method: 'POST',
+        header: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(recipeData);
+    })
+
+    const result = await response.json();
+
+    return result;
+}
