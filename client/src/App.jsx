ac4a1b64ac27b04129cd126fdd015bec5a14c783
Ventsislav Kalapchiyski <yavorov1995@gmail.com>
2023-11-23 19:13:38 +0200

    Login data passed to the app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,19 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Catalog from './components/catalog/Catalog';
 import AboutUs from './components/about/AboutUs';
-
 import Logout from './components/header/Logout';
 import Login from './components/header/Login';
 import Register from './components/header/Register';
-
 import CreateRecipe from './components/create-recipe/CreateRecipe';
 import RecipeDetails from './components/recipe-details/RecipeDetails';
+import { useState } from 'react';
 
 function App() {
+  const [auth, setAuth] = useState({});
+
+  const loginSubmitHandler = (values) => {
+    console.log(values)
+  }
 
   return (
     <>
@@ -19,7 +23,7 @@ function App() {
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/logout" element={<Logout />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login loginSubmitHandler={loginSubmitHandler}/>} />
         <Route path="/register" element={<Register />} />
         <Route path="/catalog/create-recipe" element={<CreateRecipe />} />
         <Route path="/catalog/recipes/:recipeId" element={<RecipeDetails />} />
--- client/src/components/header/Login.jsx
+++ client/src/components/header/Login.jsx
@@ -1,22 +1,46 @@
 import { Link } from "react-router-dom";
 import Layout from "../Layout";
 import styles from './Login.module.css';
+import useForm from "../../hooks/useForm";
 
-export default function Login() {
+export default function Login({
+  loginSubmitHandler,
+}) {
+
+  const { values, onChange, onSubmit } = useForm(loginSubmitHandler,{
+    email: '',
+    password: ''
+  });
+
   return (
     <Layout>
       <section className={styles.auth}>
-        <form>
+        <form onSubmit={onSubmit}>
           <div className={styles.container}>
             <div className={styles.brandLogo}></div>
             <h1>login</h1>
             <label htmlFor="email">Email:</label>
-            <input type="email" name="email" id="email" placeholder="email address" />
+            <input type="email"
+              name="email"
+              id="email"
+              placeholder="email address"
+              onChange={onChange}
+              value={values.email}
+            />
 
-            <label htmlFor="loginPass">Password:</label>
-            <input type="password" name="password" id="password" placeholder="password"/>
+            <label htmlFor="password">Password:</label>
+            <input type="password"
+              name="password"
+              id="password"
+              placeholder="password"
+              onChange={onChange}
+              value={values.password}
+            />
 
-            <input type="submit" value="Login" className={`${styles.btn} ${styles.submit}`}/>
+            <input type="submit"
+              value="Login"
+              className={`${styles.btn} ${styles.submit}`}
+            />
 
             <p className={styles.field}>
               <span>If you don't have profile, click <Link to="/register">here</Link></span>
