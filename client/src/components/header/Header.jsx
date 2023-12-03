537a97b5ab4a668146fba5f9e327084c189039c9
Ventsislav Kalapchiyski <yavorov1995@gmail.com>
2023-12-03 12:35:54 +0200

    home page fully responsive design implemented

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css';
 import Layout from '../Layout';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import AuthContext from '../../contexts/authContext';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export default function Header() {
+  library.add(faBars);
   const { isAuthenticated, username } = useContext(AuthContext);
+  const [showRespNav, setShowRespNav] = useState(false);
+
+  const handleBurgerClick = () => {
+    setShowRespNav(!showRespNav);
+  };
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -26,8 +36,27 @@ export default function Header() {
               <li><Link to="/register" className={styles.navLink}>Register</Link></li>
             </>
           }
-
-          {/* Add more navigation links as needed */}
+        </ul>
+        {/* <i className="fa fa-bars" style={{ fontSize: '36px'}}></i> */}
+        <FontAwesomeIcon
+          className={styles.burgerMenu}
+          icon={faBars}
+          onClick={handleBurgerClick}
+        />
+        <ul className={`${styles.respNavList} ${showRespNav ? '' : styles.hideOnMobile}`}>
+          <li><Link to="/about" className={styles.navLink}>About Us</Link></li>
+          {isAuthenticated
+            ?
+            <>
+              <li><Link to="/catalog" className={styles.navLink}>Catalog</Link></li>
+              <li><Link to="/logout" className={styles.navLink}>Logout</Link></li>
+              <li className={styles.username}> | {username}</li>
+            </>
+            : <>
+              <li><Link to="/login" className={styles.navLink}>Login</Link></li>
+              <li><Link to="/register" className={styles.navLink}>Register</Link></li>
+            </>
+          }
         </ul>
       </nav>
     </header>
